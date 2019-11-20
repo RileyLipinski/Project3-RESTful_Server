b9ea2f7e4396cf8441e16a818ca27bca1a111e74
Austin Hippe <hipp5302@stthomas.edu>
2019-11-19 20:52:08 -0600

    added comments to code

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
 var users;
 app.use(bodyParser.urlencoded({extended: true}));
 
+//opens the database
 var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.log('Error opening ' + db_filename);
@@ -23,12 +24,18 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
     }
 });
 
+//this app.get gets the codes from the database
 app.get('/codes', (req, res) =>
 {
     new Promise((resolve, reject) =>
     {
+        //this if handles if we have a query to access specific codes
         if(req.query.hasOwnProperty("code"))
         {
+            /*here we are dynamically creating our database call based on 
+            on how many codes were entered. we put all the codes into 
+            an array using split, and we use a for loop to put the correct
+            amount of code=? in our database call*/
             var code_array = req.query.code.split(",");
             var dbcall = "SELECT * FROM codes WHERE code=?";
             for(let i = 1; i < code_array.length; i++)
@@ -36,6 +43,8 @@ app.get('/codes', (req, res) =>
                 dbcall = dbcall + " OR code=?";
             }
             dbcall = dbcall + " ORDER BY code";
+            /*this calls our databse to get back the specified codes. Once we 
+            have the codes, we put them into a json object*/
             db.all(dbcall, code_array, (err, row) =>
             {
                 var result = {};
@@ -55,6 +64,7 @@ app.get('/codes', (req, res) =>
         }
         else
         {
+            //if specific codes are not specified, then we just do the database call and create our json object
             db.all("SELECT * FROM Codes ORDER BY code", (err, row) =>
             {
                 var result = {};
@@ -74,6 +84,8 @@ app.get('/codes', (req, res) =>
         }
     }).then((data) =>
     {
+        /*when we have the data, what we do is check to see if they want it in json or xml
+        format. Once we have converted the data to the correct form, we send it to the page*/
         if(req.query.hasOwnProperty("format") && req.query.format.toLowerCase() === "xml")
         {
             res.type("xml").send(json2xml.parse("codes", data));
@@ -88,11 +100,16 @@ app.get('/codes', (req, res) =>
     });
 });
 
-
+//this app.get gets the neighborhoods from the database
 app.get('/neighborhoods', (req, res) => 
 {
+     //this if handles if we have a query to access specific neighborhood id
     new Promise((resolve, reject) =>
     {
+        /*here we are dynamically creating our database call based on 
+            on how many ids were entered. we put all the ids into 
+            an array using split, and we use a for loop to put the correct
+            amount of neighborgood_number=? in our database call*/
         if(req.query.hasOwnProperty("id"))
         {
             var id_array = req.query.id.split(",");
@@ -102,6 +119,8 @@ app.get('/neighborhoods', (req, res) =>
                 dbcall = dbcall + " OR neighborhood_number=?";
             }
             dbcall = dbcall + " ORDER BY neighborhood_number";
+            /*this calls our databse to get back the specified neighborhood numbers. Once we 
+            have the numbers, we put them into a json object*/
             db.all(dbcall, id_array, (err, row) =>
             {
                 var result = {};
@@ -121,6 +140,7 @@ app.get('/neighborhoods', (req, res) =>
         }
         else
         {
+            //if specific ids are not specified, then we just do the database call and create our json object
             db.all("SELECT * FROM Neighborhoods ORDER BY neighborhood_number", (err, row) =>
             {
                 var result = {};
@@ -140,6 +160,8 @@ app.get('/neighborhoods', (req, res) =>
         }
     }).then((data) =>
     {
+        /*when we have the data, what we do is check to see if they want it in json or xml
+        format. Once we have converted the data to the correct form, we send it to the page*/
         if(req.query.hasOwnProperty("format") && req.query.format.toLowerCase() === "xml")
         {
             res.type("xml").send(json2xml.parse("Neighborhoods", data));
@@ -154,10 +176,18 @@ app.get('/neighborhoods', (req, res) =>
     });
 });
 
+//this app.get gets the incidents from the database
 app.get('/incidents', (req, res) =>
 {
     new Promise((resolve, reject) =>
     {
+        /*the first thing we do in this request is build our database string
+        and result array based on what queries we have. We do this in the exact
+        same manner as we did above. If we have a certain query, we push how many we
+        have to the result array, and put that many thing=? in our database string. The
+        difference is we have to check and see in each one if a where has been added to the
+        database string. If it hasnt, then we need to add one, along with how many thing=? we 
+        need.*/
         var dbcall = "SELECT * FROM Incidents";
         var result_array = [];
         var is_where = false;
@@ -283,6 +313,7 @@ app.get('/incidents', (req, res) =>
             }
         }
         dbcall = dbcall + " ORDER BY date_time DESC";
+        //Now we have our database call, where we create the json object we need to send
         db.all(dbcall, result_array, (err, row) =>
         {
             var result = {};
@@ -296,6 +327,8 @@ app.get('/incidents', (req, res) =>
             }
             else
             {
+                /*here we calculate if there has been a query for a limit. If there is no limit,
+                we default to 10000*/
                 var limit = row.length;
                 if(req.query.hasOwnProperty("limit"))
                 {
@@ -313,6 +346,7 @@ app.get('/incidents', (req, res) =>
                 {
                     if(row[i] != undefined)
                     {
+                        //what we do here is split up the date and time based on if we have one or both
                         let datetimearray = [];
                         if(row[i]["date_time"].indexOf("T") >= 0)
                         {
@@ -334,6 +368,7 @@ app.get('/incidents', (req, res) =>
                         {
                             datetimearray[1] = datetimearray[1].substring(0, datetimearray[1].indexOf("."));
                         }
+                        //this is where we create our json object
                         result["I"+row[i]["case_number"]] = 
                         {
                             "date": datetimearray[0],
@@ -351,6 +386,7 @@ app.get('/incidents', (req, res) =>
         });
     }).then((data) =>
     {
+        //this is where we determine the correct format and send the data
         if(req.query.hasOwnProperty("format") && req.query.format.toLowerCase() === "xml")
         {
             res.type("xml").send(json2xml.parse("incidents", data));
@@ -365,12 +401,16 @@ app.get('/incidents', (req, res) =>
     });
 });
 
+//this puts a new incident into our database
 app.put('/new-incident', (req, res) =>
 {
     if(req.body.hasOwnProperty("case_number") == false)
     {
         res.status(500).send("Need a case number.");
     }
+    /*what we do is create an object that holds each field we can enter to put
+    into the database, and then we have ifs that check to see if we have entered
+    one of those fields, and if we have, put it in the json object*/
     var new_obj = 
     {
         case_number: req.body.case_number,
@@ -409,6 +449,7 @@ app.put('/new-incident', (req, res) =>
     {
         new_obj["block"] = req.body.block;
     }
+    //this is where we open our database to make sure that the case number we are entering does not already exist
     db.all("SELECT * FROM Incidents WHERE case_number=?", [new_obj.case_number], (err, row) =>
     {
         if(row.length > 0)
@@ -421,6 +462,7 @@ app.put('/new-incident', (req, res) =>
         }
         else
         {
+            //if the case number does not exist, then we add the data to the database
             db.run("INSERT INTO incidents (case_number, date_time, code, incident, police_grid, neighborhood_number, block) VALUES (?, ?, ?, ?, ?, ?, ?)", [new_obj.case_number, new_obj.date_time, new_obj.code, new_obj.incident, new_obj.police_grid, new_obj.neighborhood_number, new_obj.block], (err) =>
             {
                 if(err)
